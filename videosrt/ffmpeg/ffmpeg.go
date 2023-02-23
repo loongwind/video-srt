0d8d06c37ebab19dfe7c93443b5fb366e0d8fee8
loongwind <loongwind@gmail.com>
2023-02-23 11:31:13 +0800

    change ffmpeg path

--- videosrt/ffmpeg/ffmpeg.go
+++ videosrt/ffmpeg/ffmpeg.go
@@ -3,6 +3,7 @@ package ffmpeg
 import (
 	"github.com/pkg/errors"
 	"os/exec"
+	"os"
 )
 
 type Ffmpeg struct {
@@ -12,12 +13,14 @@ type Ffmpeg struct {
 
 //提取视频音频
 func ExtractAudio (video string , tmpAudio string) (error) {
-	ts := exec.Command("ffmpeg" , "-version")
+	dir,_ := os.Getwd()
+
+	ts := exec.Command("/ffmpeg" , "-version")
 	if _ , err := ts.CombinedOutput() ; err != nil {
 		return errors.New("请先安装 ffmpeg 依赖 ，并设置环境变量")
 	}
 
-	cmd := exec.Command("ffmpeg" , "-i" , video , "-ar" , "16000" , tmpAudio)
+	cmd := exec.Command(dir+"/ffmpeg" , "-i" , video , "-ar" , "16000" , tmpAudio)
 	_, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
